f0e94e4097c83f23eed1c43032c83abb8e80c850
feng linhao <1256878002@qq.com>
2019-12-16 08:38:21 +0800

    opjjpo

--- 9.c
+++ 9.c
@@ -1,23 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 #define LEN 5
 int main(void)
 {
 
-int a[LEN]={0},i,max;
-printf("Enter a:\n");
-for(i=0;i<LEN;i++)
-{
-scanf("%d",&a[i]);
-}
-
-max=a[0];
-for(i=1;i<LEN;i++)
-{
-    if(a[i]>max){
-        max=a[i];
+    int a[LEN] = {0}, i, max;
+    printf("Enter a:\n");
+    for (i = 0; i < LEN; i++)
+    {
+        scanf("%d", &a[i]);
     }
+    max = findMax(a, LEN);
+    printf("%5d\n", max);
+    return 0;
 }
-    printf("%5d\n",max);
 
-return 0;
+int findMax(int x[], int n)
+{
+    int m, i;
+    m = x[0];
+    for (i = 1; i < n; i++)
+    {
+        if (x[i] > m)
+        {
+            m = x[i];
+        }
+    }
+    return m;
 }
